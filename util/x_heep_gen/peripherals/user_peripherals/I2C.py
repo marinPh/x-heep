df181f7d23afb34a4672ff0bf6b934bb3cd36d87
marinPh <marin.philippe@epfl.ch>
2025-12-09 22:35:13 +0100

    added the interrupts

--- util/x_heep_gen/peripherals/user_peripherals/I2C.py
+++ util/x_heep_gen/peripherals/user_peripherals/I2C.py
@@ -1,4 +1,4 @@
-from ..abstractions import UserPeripheral
+from ..abstractions import UserPeripheral, Interrupt
 
 
 class I2C(UserPeripheral):
@@ -7,3 +7,22 @@ class I2C(UserPeripheral):
     """
 
     _name = "i2c"
+
+    _interrupts = {
+        "intr_fmt_watermark": Interrupt(33, "I2C"),
+        "intr_rx_watermark": Interrupt(34, "I2C"),
+        "intr_fmt_overflow": Interrupt(35, "I2C"),
+        "intr_rx_overflow": Interrupt(36, "I2C"),
+        "intr_nak": Interrupt(37, "I2C"),
+        "intr_scl_interference": Interrupt(38, "I2C"),
+        "intr_sda_interference": Interrupt(39, "I2C"),
+        "intr_stretch_timeout": Interrupt(40, "I2C"),
+        "intr_sda_unstable": Interrupt(41, "I2C"),
+        "intr_trans_complete": Interrupt(42, "I2C"),
+        "intr_tx_empty": Interrupt(43, "I2C"),
+        "intr_tx_nonempty": Interrupt(44, "I2C"),
+        "intr_tx_overflow": Interrupt(45, "I2C"),
+        "intr_acq_overflow": Interrupt(46, "I2C"),
+        "intr_ack_stop": Interrupt(47, "I2C"),
+        "intr_host_timeout": Interrupt(48, "I2C"),
+    }
