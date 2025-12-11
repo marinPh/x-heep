5a0eac7a960c7d49001bc1c2abc121c2c5dc7abe
marinPh <marin.philippe@epfl.ch>
2025-12-11 18:00:28 +0100

    interrupt refractoring

---
+++ util/x_heep_gen/interrupts/__init__.py
@@ -0,0 +1,10 @@
+"""
+Interrupt management module for X-HEEP.
+
+This module provides the InterruptManager class for managing interrupt
+allocation, assignment, and querying in the X-HEEP system.
+"""
+
+from .interrupt_manager import InterruptManager
+
+__all__ = ["InterruptManager"]
